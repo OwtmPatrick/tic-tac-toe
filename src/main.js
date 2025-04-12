cd07763a954a38f76b0bd3d573e41b8d29ec088b
Radik Zangirov <owtmpatrick@gmail.com>
2025-04-12 07:49:34 +0500

    finish task

--- src/main.js
+++ src/main.js
@@ -1,13 +1,24 @@
 import './style.css';
+import restartLogo from '/restart.svg';
 
-const symbol = { cross: 'cross', null: 'null' };
+const symbol = { cross: 'x', null: 'o' };
+let field = new Array(9).fill(undefined);
 const tiles = Array.from(document.getElementsByClassName('field__item'));
+const result = document.getElementById('result');
+const restart = document.getElementById('restart');
 
 let currSymbol;
+let winner;
 
 function init() {
   currSymbol = symbol.cross;
-  tiles.forEach((t) => t.classList.add(currSymbol));
+  winner = undefined;
+  field = new Array(9).fill(undefined);
+
+  tiles.forEach((t) => (t.className = 'field__item cross'));
+  result.textContent = 'Result';
+  restart.textContent = 'Tic Tac Toe';
+  restart.classList.remove('info__restart');
 }
 
 function toggleTileClass() {
@@ -19,8 +30,43 @@ function toggleTileClass() {
   });
 }
 
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ];
+
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (
+      squares[a] !== undefined &&
+      squares[a] === squares[b] &&
+      squares[a] === squares[c]
+    ) {
+      return squares[a];
+    }
+  }
+  return null;
+}
+
+function setWinner(win) {
+  winner = win;
+  result.textContent = `${winner.toUpperCase()} Win`;
+  restart.innerHTML = `<img alt="restart logo" src=${restartLogo} /> Restart`;
+  restart.classList.add('info__restart');
+}
+
 function handleTileClick(e) {
+  if (winner) return;
+
   const { target } = e;
+  const tileId = target.getAttribute('data-id');
 
   if (
     target.classList.contains('crossed') ||
@@ -30,10 +76,23 @@ function handleTileClick(e) {
   }
 
   target.classList.add(currSymbol === symbol.cross ? 'crossed' : 'nulled');
-  currSymbol = currSymbol === symbol.cross ? symbol.null : symbol.cross;
+  field[tileId] = currSymbol;
+  const win = calculateWinner(field);
+
+  if (win) {
+    setWinner(win);
+    return;
+  }
+
   toggleTileClass();
+  currSymbol = currSymbol === symbol.cross ? symbol.null : symbol.cross;
+}
+
+function handleRestart() {
+  if (winner) init();
 }
 
 tiles.forEach((t) => t.addEventListener('click', handleTileClick));
+restart.addEventListener('click', handleRestart);
 
 init();
